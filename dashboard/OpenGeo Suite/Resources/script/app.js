00d4bfb61f035a7dd780c0ef16331900b263b99e
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2009-09-30 21:53:00 +0000

    Adding method for synchronous requests (until I figure out if Ext requests can be made to work).
    git-svn-id: http://svn.opengeo.org/suite/trunk@233 fe66add1-021f-4af3-b75b-ea5154e73f91

--- dashboard/OpenGeo Suite/Resources/script/app.js
+++ dashboard/OpenGeo Suite/Resources/script/app.js
@@ -1,36 +1,36 @@
 Ext.onReady(function() {
-	Ext.QuickTips.init();
+    Ext.QuickTips.init();
     
     var viewport = new Ext.Viewport({
         layout: "fit",
         items: [{
             xtype: "grouptabpanel",
-    		tabWidth: 130,
-    		activeGroup: 0,
-    		items: [{
-    			mainItem: 0,
-    			defaults: {border: false},
-    			items: [{
+            tabWidth: 130,
+            activeGroup: 0,
+            items: [{
+                mainItem: 0,
+                defaults: {border: false},
+                items: [{
                     title: "Dashboard",
                     tabTip: "OpenGeo Suite Dashboard",
                     cls: "dash-panel",
-                    html: main_content
+                    html: app.loadSync("dash/main.html")
                 }, {
-    				title: "GeoServer",
+                    title: "GeoServer",
                     tabTip: "Manage GeoServer",
                     cls: "dash-panel",
-                    html: geoserver_content
-    			}, {
-    				title: "GeoExplorer",
+                    html: app.loadSync("dash/geoserver.html")
+                }, {
+                    title: "GeoExplorer",
                     tabTip: "Manage GeoExplorer",
                     cls: "dash-panel",
-                    html: geoexplorer_content
-    			}, {
-    				title: "Styler",
+                    html: app.loadSync("dash/geoexplorer.html")
+                }, {
+                    title: "Styler",
                     tabTip: "Manage Styler",
                     cls: "dash-panel",
-                    html: styler_content
-    			}]
+                    html: app.loadSync("dash/styler.html")
+                }]
             }, {
                 items: [{
                     title: "Configuration",
@@ -41,20 +41,19 @@ Ext.onReady(function() {
                     cls: "dash-panel"
                 }]
             }]
-		}]
+        }]
     });
 });
 
 // until I figure out what is up with Titanium.Network.createHTTPClient()
-// resorting to this ugliness:
-var main_content = 
-'<div class="dash-panel-body"><div class="dash-panel-content"><div class="dash-panel-content-item"><h1>GeoServer</h1><ul class="quick-links"><li>Admin</li><li>Docs</li><li>More...</li><ul></div><div class="dash-panel-content-item"><h1>GeoExplorer</h1><ul class="quick-links"><li>Docs</li><li>More...</li><ul></div></div><div class="dash-panel-tasks"><h2>Common Tasks</h2><ul><li>Import Layers</li><li>Style Layers</li></ul><h3>Documentation</h3><ul><li>GeoServer</li><li>GeoExplorer</li><li>Styler</li></ul></div></div>';
+// resorting to this:
+var app = {
 
-var geoserver_content = 
-'<div class="dash-panel-body"><h1>GeoServer</h1><p>Describe GeoServer here.</p></div>';
+    loadSync: function(url) {
+        var client = new XMLHttpRequest();
+        client.open("GET", url, false);
+        client.send(null);
+        return client.responseText;
+    }
+};
 
-var geoexplorer_content =
-'<div class="dash-panel-body"><h1>GeoExplorer</h1><p>Describe GeoExplorer here.</p></div>';
-
-var styler_content =
-'<div class="dash-panel-body"><h1>Styler</h1><p>Describe Styler here.</p></div>';
