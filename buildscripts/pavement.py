70fd77a84cce55ee585cf36fb7a32e1c785c9273
iwillig <iwillig@fe66add1-021f-4af3-b75b-ea5154e73f91>
2009-10-05 19:29:33 +0000

    more fixes for the dashboard
    git-svn-id: http://svn.opengeo.org/suite/trunk@255 fe66add1-021f-4af3-b75b-ea5154e73f91

--- buildscripts/pavement.py
+++ buildscripts/pavement.py
@@ -426,9 +426,7 @@ def dashboard():
 		if sys.platform == 'linux2': 
 			sh("tibuild.py -v -d . -n -t bundle  -s %s -a %s ." % (TIBUILD, TIENV))
 	 	if sys.platform == 'win32': 
-			sh("""tibuild.py  -v -d . -s "C:\Documents and Settings\All Users\Application Data\Titanium" 
-				-a "C:\Documents and Settings\All Users\Application Data\Titanium\sdk\win32\0.6.0" 
-				. -n -t bundle""")
+			sh("tibuild.py  -v -d . -s \"C:\Documents and Settings\All Users\Application Data\Titanium\" -a \"C:\Documents and Settings\All Users\Application Data\Titanium\sdk\win32\0.6.0\" . -n -t bundle")
 		else: 
 		  	# What do we on OS X ? 
 			pass 
@@ -438,11 +436,11 @@ def dashboard():
 		with pushd(source_path): 
 			sh("gunzip OpenGeo\ Suite.tgz ; tar -xf OpenGeo\ Suite.tar")
 			os.remove("OpenGeo Suite.tar")
-	if sys.platform == 'win33':
+	if sys.platform == 'win32':
 		copy(path.joinpath(dashboard,"OpenGeo Suite.exe"),source_path)
    		with pushed(source_path): 
 			unzip_file("OpenGeo Suite.exe")
-			os.remove("OpenGeo Suite.ext") 
+			os.remove("OpenGeo Suite.exe") 
 
 @task
 def source_dirs(): 
