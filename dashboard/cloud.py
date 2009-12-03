05a4bd03b5138babd2b706ac5de4a44b4ea7df64
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2009-12-03 19:39:00 +0000

    Waiting for job completion.  Not yet downloading packages.
    git-svn-id: http://svn.opengeo.org/suite/trunk@465 fe66add1-021f-4af3-b75b-ea5154e73f91

--- dashboard/cloud.py
+++ dashboard/cloud.py
@@ -3,28 +3,32 @@
 # This script is used to package a Titanium application in the cloud.
 #
 
-import sys, os, logging, urllib, urllib2, json, zipfile, StringIO
+import sys, os, logging, time, urllib, urllib2, json, zipfile, StringIO
 
-logger = logging.getLogger("cloud")
+logger = logging.getLogger("titanium.cloud")
 cloud_url = "https://api.appcelerator.net/p/v1/"
 
 def login(app_path, user, password):
 
-    url = cloud_url + "sso-login"
+    url = cloud_url + "sso-login"    
 
     # get some app details
     h = open(os.path.join(app_path, "timanifest"))
     manifest = json.loads(h.read())
     h.close()
 
+    logger.info("Logging in to %s", url)
     data = urllib.urlencode({"mid": manifest["mid"], "un": user, "pw": password})
     h = urllib.urlopen(url, data)
     response = h.read()
     h.close()
     
     details = json.loads(response)
-    if not details["success"]:
-        logger.warning("login failed: %s", response)
+    
+    if details["success"]:
+        logger.info("Successfully logged in.")
+    else:
+        logger.warning("Login failed: %s", response)
 
     return details
 
@@ -37,7 +41,7 @@ def bundle(app_path, ignore=(".svn",)):
                 entry = os.path.join(path, entry)
                 add_entry(entry, archive)
         elif path not in ignore:
-            archive.write(path, path[len(app_path)+1:])
+            archive.write(path, os.path.relpath(path, app_path))
         return
     
     entries = ("Resources", "modules", "timanifest", "manifest",
@@ -47,6 +51,7 @@ def bundle(app_path, ignore=(".svn",)):
     archive = zipfile.ZipFile(zip_data, "w", zipfile.ZIP_DEFLATED)    
     for entry in entries:
         path = os.path.join(app_path, entry)
+        logger.info("Bundling %s", path)
         if os.path.exists(path):
             add_entry(path, archive)
     archive.close()
@@ -70,40 +75,76 @@ def package(zip_data, sid=None, token=None, uid=None, uidt=None):
         "Content-Length": str(len(data))
     }
     
+    logger.info("Uploading app archive to %s", url)
     req = urllib2.Request("%s?%s" % (url, params), data, headers)
     h = urllib2.urlopen(req)
     response = h.read()
     h.close()
     
     details = json.loads(response)
-    if not details["success"]:
-        logger.warning("packaging failed: %s", response)
+    if details["success"]:
+        logger.info("App archive accepted, package pending.")
+    else:
+        logger.warning("App archive rejected:\n%s", response)
     
     return details["ticket"]
 
 
-def check_status(ticket):
+def get_status(ticket):
     
+    logger.info("Checking status for ticket %s", ticket)
     url = cloud_url + "publish-status"
     params = urllib.urlencode({"ticket": ticket})
-    h = urllib.urlopen("%s?%s", (url, params))
+    h = urllib.urlopen("%s?%s" % (url, params))
     response = h.read()
     h.close()
 
     details = json.loads(response)
-    if details["success"] == False:
-        logger.warning("packaging failed: %s", response)
+    return details
+
+
+def wait(ticket, interval=30, timeout=300, start=None):
+
+    if start is None:
+        start = time.time()
+    
+    details = get_status(ticket)
+    current = time.time()
     
     if details["status"] == "complete":
-        # deal with success
+        logger.info("Packaging complete.")
         pass
-    elif details["success"] == False:
-        # deal with failure
+    elif details.has_key("success"):
+        if details["success"] == False:
+            logger.warning("Packaging failed: %s", details["message"])
+        else:
+            logger.warning("Inconsistent response: %s", str(details))
         pass
     else:
-        # keep waiting (check_status again later)
-        pass
+        # keep waiting (get_status again later) unless timeout exceeded
+        if current - start > timeout:
+            logger.warning("Maximum wait time of %d seconds exceeded.  Giving up waiting on ticket %s.", timeout, ticket)
+        else:
+            logger.info("Packaging not yet complete.  Checking again in %d seconds.", interval)
+            time.sleep(interval)
+            details = wait(ticket, interval=interval, timeout=timeout, start=start)
+    
+    return details
 
+def download(releases, dir=os.getcwd()):
+    
+    # only grab one per os (service occassionally returns two entries for same os)
+    platforms = {}
+    for entry in releases:
+        platforms[entry["platform"]] = entry["url"]
+    
+    # download each
+    for platform, url in platforms.items():
+        logger.info("Downloading %s package %s", platform, url)
+        #h = urllib.urlopen(url)
+        #data = h.read()
+        #h.close()
+        # write data to dir
 
 def main():
 
@@ -123,8 +164,9 @@ def main():
         help="PASSWORD for the USER"
     )
     parser.add_option(
-        "-v", "--verbose",
-        help="print all messages"
+        "-q", "--quiet",
+        action="store_false", dest="verbose", default=True,
+        help="don't print status messages"
     )
     
     group = OptionGroup(
@@ -138,17 +180,22 @@ def main():
     parser.add_option_group(group)
 
     (options, args) = parser.parse_args()
-    
+        
     if not len(args) == 1:
         parser.error("You must provide the path to your application.  Run with -h for help.")
+    else:
+        app_path = args[0]
+    
+    if not os.path.exists(app_path):
+        parser.error("Can't find application %s." % (app_path,))
 
     # add handler for console to logger
-    logger = logging.getLogger("cloud")
+    logger = logging.getLogger("titanium.cloud")
     console_handler = logging.StreamHandler()
     if options.verbose:
-        console_handler.setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
     else:
-        console_handler.setLevel(logging.ERROR)
+        logger.setLevel(logging.ERROR)
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
@@ -156,7 +203,7 @@ def main():
     # gather app details
     details = {}
     if options.user and options.password:
-        details = login(app_path, user, password)
+        details = login(app_path, options.user, options.password)
     else:
         if not (options.sid and options.token and options.uid and options.uidt):
             parser.error("You must provide either USER and PASSWORD or all of SID, TOKEN, UID, and UIDT. Run with -h for help.")
@@ -169,10 +216,15 @@ def main():
     # bundle up app resources as a zip
     zip_data = bundle(app_path)
     
-    # post app bundle for packaging
+    # post app bundle for packaging (can't use **details here given unicode keys)
     ticket = package(zip_data, sid=details['sid'], token=details['token'], uid=details['uid'], uidt=details['uidt'])
     
     # keep checking status until complete
+    job = wait(ticket)
+
+    # download all releases
+    download(job["releases"])        
+
 
 if __name__ == "__main__":
     main()
