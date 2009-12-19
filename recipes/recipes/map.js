8f2693a950bbea47e54f19c4d63e9f7bba8c4cc2
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2009-12-19 00:10:37 +0000

    Adding a basic map example.
    git-svn-id: http://svn.opengeo.org/suite/trunk@529 fe66add1-021f-4af3-b75b-ea5154e73f91

---
+++ recipes/recipes/map.js
@@ -0,0 +1,21 @@
+// the `map` variable is declared here for easy debugging
+var map;
+
+// this function is assigned to the window load event
+function init() {
+
+    // create a new map to be drawn in the page element with id "map-id"
+    map = new OpenLayers.Map("map-id");
+    
+    // create a WMS layer and add it to the map
+    var layer = new OpenLayers.Layer.WMS(
+        "Global Imagery",
+        "http://maps.opengeo.org/geowebcache/service/wms",
+        {layers: "bluemarble"}
+    );
+    map.addLayer(layer);
+
+    // set the map location
+    map.zoomToMaxExtent();
+
+};
