0018baf79d130e7b5ca1028cbcd36057ec175a81
jdeolive <jdeolive@fe66add1-021f-4af3-b75b-ea5154e73f91>
2009-12-20 22:37:19 +0000

    added custom startup class to jetty module for reading config.ini file before starting the server
    git-svn-id: http://svn.opengeo.org/suite/trunk@533 fe66add1-021f-4af3-b75b-ea5154e73f91

---
+++ jetty/src/main/java/org/opengeo/jetty/Start.java
@@ -0,0 +1,42 @@
+package org.opengeo.jetty;
+
+import java.io.File;
+
+import org.ini4j.Wini;
+
+/**
+ * Wrapper around Jetty main which reads the configuration file prior to starting jetty.
+ * 
+ * @author Justin Deoliveira, OpenGeo
+ *
+ */
+public class Start {
+
+    public static void main(String[] args) throws Exception {
+        
+        File f = new File(
+            System.getProperty("user.home")+File.separator+".opengeo"+File.separator+"config.ini");
+        if (f.exists()) {
+            Wini ini = new Wini(f);
+            
+            String startPort = ini.get("suite", "start_port");
+            if (startPort != null) {
+                System.setProperty("jetty.port", startPort);
+            }
+            
+            String stopPort = ini.get("suite", "stop_port");
+            if (stopPort != null) {
+                System.setProperty("STOP.PORT", stopPort);
+            }
+        }
+        else {
+            System.out.println(f.getAbsolutePath() + " not found, starting with default parameters.");
+        }
+        
+        if (System.getProperty("STOP.PORT") == null) {
+            System.setProperty("STOP.PORT", "8079");
+        }
+        System.setProperty("STOP.KEY", "opengeo");
+        org.mortbay.start.Main.main(args);
+    }
+}
