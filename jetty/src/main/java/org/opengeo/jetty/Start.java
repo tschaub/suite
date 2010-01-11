797a616438c784b14bc593ee61707b460d0be776
jdeolive <jdeolive@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-01-11 03:21:07 +0000

    moved more configuration to og startup class, tweaked initial log configuration
    git-svn-id: http://svn.opengeo.org/suite/trunk@582 fe66add1-021f-4af3-b75b-ea5154e73f91

--- jetty/src/main/java/org/opengeo/jetty/Start.java
+++ jetty/src/main/java/org/opengeo/jetty/Start.java
@@ -14,29 +14,43 @@ public class Start {
 
     public static void main(String[] args) throws Exception {
         
+        Wini ini = null;
         File f = new File(
             System.getProperty("user.home")+File.separator+".opengeo"+File.separator+"config.ini");
+        
         if (f.exists()) {
-            Wini ini = new Wini(f);
-            
-            String startPort = ini.get("?", "port");
-            if (startPort != null) {
-                System.setProperty("jetty.port", startPort);
-            }
-            
-            String stopPort = ini.get("?", "stop_port");
-            if (stopPort != null) {
-                System.setProperty("STOP.PORT", stopPort);
-            }
+            ini = new Wini(f);
         }
         else {
-            System.out.println(f.getAbsolutePath() + " not found, starting with default parameters.");
+            ini = new Wini();
+            System.out.println(f.getAbsolutePath() + " not found, continuing with default parameters.");
         }
         
-        if (System.getProperty("STOP.PORT") == null) {
-            System.setProperty("STOP.PORT", "8079");
+        String startPort = ini.get("?", "port");
+        if (startPort != null) {
+            System.setProperty("jetty.port", startPort);
         }
+        
+        String stopPort = ini.get("?", "stop_port");
+        if (stopPort == null) {
+            stopPort = "8079";
+        }
+        System.setProperty("STOP.PORT", stopPort);
         System.setProperty("STOP.KEY", "opengeo");
+        
+        String gsDataDirectory = ini.get("geoserver", "data_dir" );
+        if (gsDataDirectory == null) {
+            File dd = new File("data_dir");
+            if (dd.exists()) {
+                gsDataDirectory = dd.getCanonicalPath();
+            }
+        }
+        if (gsDataDirectory != null) {
+            System.setProperty("GEOSERVER_DATA_DIR", gsDataDirectory);    
+        }
+        
+        System.setProperty("RELINQUISH_LOG4J_CONTROL", "true");
+        
         org.mortbay.start.Main.main(args);
     }
 }
