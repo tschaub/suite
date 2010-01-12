93dfbefd2128adca78d834b8c8b03cf4570aa84a
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-01-12 18:20:20 +0000

    Bumping up the rule dialog width for IE (closes #246).
    git-svn-id: http://svn.opengeo.org/suite/trunk@612 fe66add1-021f-4af3-b75b-ea5154e73f91

--- styler/lib/Styler.js
+++ styler/lib/Styler.js
@@ -851,7 +851,7 @@ var Styler = Ext.extend(Ext.util.Observable, {
             layout: "fit",
             x: this.windowPositions.ruleDlg.x,
             y: this.windowPositions.ruleDlg.y,
-            width: 265,
+            width: 270,
             constrainHeader: true,
             items: [{
                 xtype: "gx_rulepanel",
