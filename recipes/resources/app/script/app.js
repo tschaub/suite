51cd417d682e70d68bd5f93a97454496e0203120
awright <awright@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-01-18 02:29:17 +0000

    small update to north region, recipe-link html.
    git-svn-id: http://svn.opengeo.org/suite/trunk@700 fe66add1-021f-4af3-b75b-ea5154e73f91

--- recipes/resources/app/script/app.js
+++ recipes/resources/app/script/app.js
@@ -152,7 +152,7 @@ og.Recipes = Ext.extend(Ext.util.Observable, {
                 autoEl: {
                     tag: "div",
                     cls: "header",
-                    html: "<h1><a class='recipe-link' href='#index'>OpenGeo Recipe Book</a></h1>"
+                    html: "<a class='recipe-link' href='#index'></a>"
                 }
             }, {
                 region: "west",
