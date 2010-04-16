ced99f202fcc7db47b38adc4e7edb0df2a0e54c6
pramsey <pramsey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-04-16 23:36:17 +0000

    Update uninstaller
    git-svn-id: http://svn.opengeo.org/suite/trunk@966 fe66add1-021f-4af3-b75b-ea5154e73f91

--- installer/mac.new/binaries/suite/bin/suite_uninstall.sh
+++ installer/mac.new/binaries/suite/bin/suite_uninstall.sh
@@ -4,12 +4,15 @@
 if [ -d /Applicatons/OpenGeo ]; then
   rm -rf /Applications/OpenGeo
 fi
+
 # Remove Server Apps
 if [ -d /opt/opengeo ]; then
   rm -rf /opt/opengeo/
 fi
+
 # Remove Config Files
 find /Users -name .opengeo -maxdepth 2 -type d -exec /bin/rm -rf {} ';'
+
 # Remove Path Entries
 if [ -f /private/etc/paths.d/opengeo-pgsql ]; then
   rm /private/etc/paths.d/opengeo-pgsql
@@ -17,4 +20,8 @@ fi
 if [ -f /private/etc/manpaths.d/opengeo-pgsql ]; then
   rm /private/etc/manpaths.d/opengeo-pgsql
 fi
+
+# Remove receipts entries
+find /Library/Receipts -name "org.opengeo.*" -exec /bin/rm -rf {} ';'
+find /var/db/receipts -name "org.opengeo.*" -exec /bin/rm -rf {} ';'
 
