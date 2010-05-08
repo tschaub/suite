70de6e18daa2dc49f64bb85f38335eb1bb691ef3
pramsey <pramsey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-08 14:58:57 +0000

    Remove proj and geos lines from master build, hudson will run those separately
    git-svn-id: http://svn.opengeo.org/suite/trunk@1078 fe66add1-021f-4af3-b75b-ea5154e73f91

--- build/osx/hudson_master.sh
+++ build/osx/hudson_master.sh
@@ -12,24 +12,6 @@ if [ $# -ne 0 ]; then
   usage
 fi
 
-# Build proj
-echo "Running hudson_proj.sh..."
-${d}/hudson_proj.sh proj-svn
-rv=$?
-if [ $rv -gt 0 ]; then
-  echo "hudson_proj failed with return value $rv"
-  exit $rv
-fi
-
-# Build geos
-echo "Running hudson_geos.sh..."
-${d}/hudson_geos.sh geos-svn
-rv=$?
-if [ $rv -gt 0 ]; then
-  echo "hudson_geos failed with return value $rv"
-  exit $rv
-fi
-
 # Build postgis
 echo "Running hudson_postgis.sh..."
 ${d}/hudson_postgis.sh postgis-svn
