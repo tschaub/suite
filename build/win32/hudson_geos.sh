c8f5f277147bb02706a7b270298fc16272afdd3c
pramsey <pramsey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-10 21:28:17 +0000

    Add stubs for hudson building of suite
    git-svn-id: http://svn.opengeo.org/suite/trunk@1088 fe66add1-021f-4af3-b75b-ea5154e73f91

---
+++ build/win32/hudson_geos.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Script directory
+d=`dirname $0`
+
+# Load versions
+source ${d}/hudson_config.sh
+
+function usage() {
+  echo "Usage: $0 <srcdir>"
+  exit 1
+}
+
+if [ $# -lt 1 ]; then
+  usage
+fi
+
+srcdir=$1
+
+if [ ! -d $srcdir ]; then
+  exit 1
+else 
+  pushd $srcdir
+fi
+
+./autogen.sh
+./configure --prefix=${buildroot}/geos 
+make clean && make all
+checkrv $? "GEOS build"
+
+rm -rf ${buildroot}/geos
+mkdir ${buildroot}/geos
+make install
+pushd ${buildroot}/geos
+rm -f ${webroot}/geos-win.zip
+zip -r9 ${webroot}/geos-win.zip *
+checkrv $? "GEOS zip"
+popd
+
+popd
+
+exit 0
+    
---
+++ build/win32/hudson_proj.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Script directory
+d=`dirname $0`
+
+# Load versions
+source ${d}/hudson_config.sh
+
+function usage() {
+  echo "Usage: $0 <srcdir>"
+  exit 1
+}
+
+if [ $# -lt 1 ]; then
+  usage
+fi
+
+srcdir=$1
+
+if [ ! -d $srcdir ]; then
+  exit 1
+else
+  pushd $srcdir
+fi
+
+# Unsip the 
+if [ ! -f ${buildroot}/${proj_nad} ]; then
+  curl http://download.osgeo.org/proj/${proj_nad} > ${buildroot}/${proj_nad}
+fi
+pushd nad
+unzip -o ${buildroot}/${proj_nad}
+popd
+
+./configure --prefix=${buildroot}/proj 
+make clean all
+checkrv $? "Proj build"
+
+rm -rf ${buildroot}/proj
+mkdir ${buildroot}/proj
+make install
+
+pushd ${buildroot}/proj
+rm -f ${webroot}/proj-win.zip
+zip -r9 ${webroot}/proj-win.zip *
+checkrv $? "Proj zip"
+popd
+
+popd
+
+exit 0
+    
