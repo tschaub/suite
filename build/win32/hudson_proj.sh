0e29c2072392740eabb06cf2073be048f0ba9246
pramsey <pramsey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-11 16:20:09 +0000

    Update the proj build to hopefully complete cleanly
    git-svn-id: http://svn.opengeo.org/suite/trunk@1096 fe66add1-021f-4af3-b75b-ea5154e73f91

--- build/win32/hudson_proj.sh
+++ build/win32/hudson_proj.sh
@@ -19,35 +19,47 @@ fi
 
 srcdir=$1
 
+# Enter the source directory
 if [ ! -d $srcdir ]; then
   exit 1
 else
   pushd $srcdir
 fi
 
-# Unsip the 
+# Fetch the NAD grids files
 if [ ! -f ${buildroot}/${proj_nad} ]; then
   curl http://download.osgeo.org/proj/${proj_nad} > ${buildroot}/${proj_nad}
 fi
 pushd nad
+# Unzip the NAD grids files
+if [ ! -f ${buildroot}/${proj_nad} ]; then
 unzip -o ${buildroot}/${proj_nad}
 popd
 
+# Patch a current build issue with MinGW and proj
+patch -p0 < ../suite-build/proj_mutex.patch
+checkrv $? "Proj mutex patch"
+
+# Build proj
 ./configure --prefix=${buildroot}/proj --without-mutex
 make clean all
 checkrv $? "Proj build"
 
+# Clean the install directory
 rm -rf ${buildroot}/proj
 mkdir ${buildroot}/proj
 make install
 
+# Zip up the artifacts for posterity
 pushd ${buildroot}/proj
 rm -f ${webroot}/proj-win.zip
 zip -r9 ${webroot}/proj-win.zip *
 checkrv $? "Proj zip"
 popd
 
+# Leave the source directory
 popd
 
+# Done
 exit 0
     
