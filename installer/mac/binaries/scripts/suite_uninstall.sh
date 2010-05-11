8e15bd2b491a74f0022f0eed6edc9283d4f03336
pramsey <pramsey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-11 23:56:53 +0000

    More verbosity :)
    git-svn-id: http://svn.opengeo.org/suite/trunk@1121 fe66add1-021f-4af3-b75b-ea5154e73f91

--- installer/mac/binaries/scripts/suite_uninstall.sh
+++ installer/mac/binaries/scripts/suite_uninstall.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
+echo ""
+echo "Removing OpenGeo Suite files..."
+echo ""
+
 # Remove GUI Apps
 if [ -d /Applications/OpenGeo ]; then
-  rm -rf /Applications/OpenGeo
+  rm -rvf /Applications/OpenGeo
 fi
 
 # Remove Server Apps
 if [ -d /opt/opengeo ]; then
-  rm -rf /opt/opengeo
+  rm -rvf /opt/opengeo
 fi
 
 # Remove Config Files
-find /Users -name .opengeo -maxdepth 2 -type d -exec /bin/rm -rf {} ';'
+find /Users -name .opengeo -maxdepth 2 -type d -exec /bin/rm -rvf {} ';'
 
 # Remove Path Entries
 if [ -f /private/etc/paths.d/opengeo-pgsql ]; then
-  rm /private/etc/paths.d/opengeo-pgsql
+  rm -vf /private/etc/paths.d/opengeo-pgsql
 fi
 if [ -f /private/etc/manpaths.d/opengeo-pgsql ]; then
-  rm /private/etc/manpaths.d/opengeo-pgsql
+  rm -vf /private/etc/manpaths.d/opengeo-pgsql
 fi
 
 # Remove receipts entries
 if [ -d /Library/Receipts ]; then
-  find /Library/Receipts -name "org.opengeo.*" -exec /bin/rm -rf {} ';'
+  find /Library/Receipts -name "org.opengeo.*" -exec /bin/rm -rvf {} ';'
 fi
 if [ -d /var/db/receipts ]; then
-  find /var/db/receipts -name "org.opengeo.*" -exec /bin/rm -rf {} ';'
+  find /var/db/receipts -name "org.opengeo.*" -exec /bin/rm -rvf {} ';'
 fi
 
+echo ""
 echo "The OpenGeo Suite is now uninstalled."
+echo ""
 
