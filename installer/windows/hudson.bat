59c1791f23ef2d372471d23f50bd7e3b73bdfa7c
mpumphrey <mpumphrey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-12 19:32:48 +0000

    Updates to windows hudson
    git-svn-id: http://svn.opengeo.org/suite/trunk@1129 fe66add1-021f-4af3-b75b-ea5154e73f91

--- installer/windows/hudson.bat
+++ installer/windows/hudson.bat
@@ -1,9 +1,8 @@
 REM job to build .EXE
 REM assumes that http://svn.opengeo.org/suite/trunk/installer has been checked out
 
-wget http://suite.opengeo.org/builds/opengeosuite-latest-win.zip
-mkdir ..\target\opengeosuite-1.0-win
-unzip opengeosuite-latest-win.zip ..\target\opengeosuite-1.0-win
+wget http://suite.opengeo.org/builds/opengeosuite-latest-win.zip >nul
+mkdir ..\target\opengeosuite-1.0-win 2>nul
+unzip opengeosuite-latest-win.zip ..\..\target\opengeosuite-1.0-win
 del opengeosuite-latest-win.zip
-cd windows
 makensis OpenGeoInstaller.nsi
