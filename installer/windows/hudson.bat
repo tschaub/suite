dc1502a1981218efecb47c5b1275fd79a1865453
mpumphrey <mpumphrey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-12 20:55:14 +0000

    Updates to windows hudson script.
    git-svn-id: http://svn.opengeo.org/suite/trunk@1132 fe66add1-021f-4af3-b75b-ea5154e73f91

--- installer/windows/hudson.bat
+++ installer/windows/hudson.bat
@@ -1,15 +1,21 @@
 REM job to build .EXE
 REM assumes that http://svn.opengeo.org/suite/trunk/installer has been checked out
 
-wget http://suite.opengeo.org/builds/opengeosuite-latest-win.zip >nul
+REM Get the maven artifact in place
+@echo Downloading http://suite.opengeo.org/builds/opengeosuite-latest-win.zip ...
+wget http://suite.opengeo.org/builds/opengeosuite-latest-win.zip >nul 2>nul
 mkdir ..\..\target\opengeosuite-1.0-win 2>nul
-unzip opengeosuite-latest-win.zip ..\..\target\opengeosuite-1.0-win
+unzip opengeosuite-latest-win.zip -d ..\..\target\opengeosuite-1.0-win
 del opengeosuite-latest-win.zip
 
-wget http://suite.opengeo.org/builds/dashboard-latest-win32.zip >nul
-unzip dashboard-latest-win32.zip ..\..\target\opengeosuite-1.0-win\dashboard
+REM Get the dashboard in place
+@echo Downloading http://suite.opengeo.org/builds/dashboard-latest-win32.zip ...
+wget http://suite.opengeo.org/builds/dashboard-latest-win32.zip >nul 2>nul
+unzip dashboard-latest-win32.zip -d ..\..\target\opengeosuite-1.0-win\dashboard
 del dashboard-latest-win32.zip
 rd /s /q ..\..\target\opengeosuite-1.0-win\dashboard\assembly
 
 makensis OpenGeoInstaller.nsi
 
+REM Clean up
+rd /s /q ..\..\target\
