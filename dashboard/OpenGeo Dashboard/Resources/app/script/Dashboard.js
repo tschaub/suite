3771dff1091fdd2f50468d0fc0133d315bb471b9
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-18 22:28:19 +0000

    Removing unused sidebar items.
    git-svn-id: http://svn.opengeo.org/suite/trunk@1190 fe66add1-021f-4af3-b75b-ea5154e73f91

--- dashboard/OpenGeo Dashboard/Resources/app/script/Dashboard.js
+++ dashboard/OpenGeo Dashboard/Resources/app/script/Dashboard.js
@@ -215,44 +215,7 @@ og.Dashboard = Ext.extend(Ext.util.Observable, {
                         cls: "dash-panel",
                         id: "app-panels-help-logs",
                         items: [this.logPanel]
-                    } //,  {     
-                        //     title: "GeoExplorer",
-                        //     tabTip: "Learn about GeoExplorer",
-                        //     cls: "dash-panel",
-                        //     html: og.util.loadSync("app/markup/dash/geoexplorer.html"),
-                        //     id: "app-panels-dash-geoexplorer"
-                        // },  {
-                        //     title: "GeoEditor",
-                        //     tabTip: "Learn about GeoEditor",
-                        //     cls: "dash-panel",
-                        //     html: og.util.loadSync("app/markup/dash/geoeditor.html"),
-                        //     id: "app-panels-dash-geoeditor"
-                        // },  {
-                        //     title: "Styler",
-                        //     tabTip: "Learn about Styler",
-                        //     cls: "dash-panel",
-                        //     html: og.util.loadSync("app/markup/dash/styler.html"),
-                        //     id: "app-panels-dash-styler"
-                        // },  {
-                        //     title: "GeoServer",
-                        //     tabTip: "Learn about GeoServer",
-                        //     cls: "dash-panel",
-                        //     html: og.util.loadSync("app/markup/dash/geoserver.html"),
-                        //     id: "app-panels-dash-geoserver"
-                        // }, {
-                        //     title: "GeoWebCache",
-                        //     tabTip: "Learn about GeoWebCache",
-                        //     cls: "dash-panel",
-                        //     html: og.util.loadSync("app/markup/dash/geowebcache.html"),
-                        //     id: "app-panels-dash-geowebcache"
-                        // }, {
-                        //     title: "PostGIS",
-                        //     tabTip: "Learn about PostGIS",
-                        //     cls: "dash-panel",
-                        //     html: og.util.loadSync("app/markup/dash/postgis.html"),
-                        //     id: "app-panels-dash-postgis"
-                        // }
-                        ]
+                    }]
                 }, {
                     defaults: {border: false, autoScroll: true},
                     items: [{
