2e0f91d00bc1fd2a9589f496b5af9e3e02039879
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-05-20 17:32:19 +0000

    Simplified log reading worker (see #758).
    git-svn-id: http://svn.opengeo.org/suite/trunk@1246 fe66add1-021f-4af3-b75b-ea5154e73f91

--- dashboard/OpenGeo Dashboard/Resources/app/script/log.js
+++ dashboard/OpenGeo Dashboard/Resources/app/script/log.js
@@ -4,57 +4,31 @@
 
 function readBuffer(file) {
     var buf = [];
-    for (var line = file.readLine(); line !== null; line=file.readLine()) {
-        buf.push(line);
-        // only keep the last 1000 lines
-        if (buf.length > 1000) {
-            buf.shift();
-        }
+    try {
+        for (var line = file.readLine(); line !== null; line=file.readLine()) {
+            buf.push(line.toString());
+            // only keep the last 1000 lines
+            if (buf.length > 1000) {
+                buf.shift();
+            }
+        }        
+    } catch (err) {
+        buff = ["Unable to read log file: '" + file.nativePath() + "'", err.message];
     }
     if (buf.length === 0) {
-        buf = ["Could not read log file."];
+        buf = ["Empty log file."];
     }
     return buf.join("\n");
-}
-
-function readOffset(file) {
-     var offset = file.size() - 100*1024;
-     var buf = [];
-     var count = 0;
-              
-     file = Titanium.Filesystem.getFileStream(file.nativePath());
-     if (file.open(Titanium.Filesystem.MODE_READ) == true) {
-          var line = file.readLine();
-
-          //skip over lines until we get to the last 100K
-          while(line != null && count < offset) {
-              count += line.length+1;
-              line = file.readLine();
-          }
-
-          while(line != null) {
-              buf.push(line);
-              line = file.readLine();
-          }
-          file.close();    
-     }
-     else {
-         postMessage("Could not read log file");
-     }
 }
 
 function readFile(path) {
     var file = Titanium.Filesystem.getFile(path);
     var message;
     if (file.exists() === true) {
-        //readOffset(file);
         message = readBuffer(file);
+    } else {
+        message = "Can't find log file: '" + path + "'";
     }
-
-    if (message == null) {
-        message = "Unable to read log from '" + path + "'.";
-    }
-
     postMessage(message);    
 }
 
