a1625c5dd72df3c43b75b269cf435ef7aad8bc08
pramsey <pramsey@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-06-21 23:57:36 +0000

    Add in glib and gtk build actions to bundle
    git-svn-id: http://svn.opengeo.org/suite/trunk@1615 fe66add1-021f-4af3-b75b-ea5154e73f91

--- build/linux/hudson_bundle.sh
+++ build/linux/hudson_bundle.sh
@@ -33,7 +33,7 @@ fi
 
 
 # Copy in other files
-for d in openssl geos proj pgadmin wxwidgets; do
+for d in openssl geos proj pgadmin wxwidgets glib gtk; do
   cp -r ${buildroot}/${d}/* ${buildroot}/pgsql
 done
 
