ca40638a277a83e1e1194c726db2b3ce32ff4334
awright <awright@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-08-19 20:13:14 +0000

    changed default value of importer to OGS rather then OGS.  (closes #654) for real.
    git-svn-id: http://svn.opengeo.org/suite/trunk@1737 fe66add1-021f-4af3-b75b-ea5154e73f91

--- geoserver/importer/src/main/java/org/geoserver/web/importer/GeneralStoreParamPanel.java
+++ geoserver/importer/src/main/java/org/geoserver/web/importer/GeneralStoreParamPanel.java
@@ -27,7 +27,7 @@ import org.geoserver.web.wicket.GeoServerDialog.DialogDelegate;
 @SuppressWarnings("serial")
 class GeneralStoreParamPanel extends Panel {
     WorkspaceDetachableModel workspace;
-    String name = "ogc";
+    String name = "ogs";
     String description;
     GeoServerDialog dialog;
     private DropDownChoice wsChoice;
