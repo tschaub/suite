ec191de558a10752c1a15587735efd098d7d9bc5
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2010-12-01 01:13:52 +0000

    Avoiding unnecessary redirects while collecting stats.
    git-svn-id: http://svn.opengeo.org/suite/trunk@1939 fe66add1-021f-4af3-b75b-ea5154e73f91

--- dashboard/OpenGeo Dashboard/Resources/app/script/workers/stats.js
+++ dashboard/OpenGeo Dashboard/Resources/app/script/workers/stats.js
@@ -74,7 +74,7 @@ function getLayerCount(config) {
 
 function getMapCount(config) {
     var resource = getResource(
-        "http://" + config["suite_host"] + ":" + config["suite_port"] + "/geoexplorer/maps"
+        "http://" + config["suite_host"] + ":" + config["suite_port"] + "/geoexplorer/maps/"
     );
     return resource && resource.ids.length;
 }
