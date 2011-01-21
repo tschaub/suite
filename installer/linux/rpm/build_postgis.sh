a160e0164a870b4636dd9c4ef6f6c9cf657a7829
jdeolive <jdeolive@fe66add1-021f-4af3-b75b-ea5154e73f91>
2011-01-21 22:57:38 +0000

    scripts for proj, geos, and postgis
    git-svn-id: http://svn.opengeo.org/suite/trunk@2022 fe66add1-021f-4af3-b75b-ea5154e73f91

---
+++ installer/linux/rpm/build_postgis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. functions
+
+build_info
+
+POSTGIS=postgis-1.5.2
+
+# grab files
+get_file http://postgis.refractions.net/download/$POSTGIS.tar.gz
+cp files/$POSTGIS.tar.gz $RPM_SOURCE_DIR
+
+# build
+build_rpm yes
