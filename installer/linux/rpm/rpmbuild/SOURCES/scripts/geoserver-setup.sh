8449cd952fe7825614009479f733850e2236869c
jdeolive <jdeolive@fe66add1-021f-4af3-b75b-ea5154e73f91>
2011-01-25 02:51:14 +0000

    #1177, allowing geoserver-setup to be run multiple times without wiping out old configuration
    git-svn-id: http://svn.opengeo.org/suite/trunk@2039 fe66add1-021f-4af3-b75b-ea5154e73f91

--- installer/linux/rpm/rpmbuild/SOURCES/scripts/geoserver-setup.sh
+++ installer/linux/rpm/rpmbuild/SOURCES/scripts/geoserver-setup.sh
@@ -3,9 +3,28 @@
 #
 
 fin=0
-myhost=`echo $(/sbin/ifconfig eth0 | awk -F: '/inet addr:/ {print $2}' | awk '{ print $1 }')`
-username="admin"
-password="admin"
+
+# get current values from geoserver_data
+GS_DATA=/usr/share/opengeo-suite-data/geoserver_data
+GS_DATA=/Users/jdeolive/Projects/gs_trunk/svn/data/release
+
+if [ -e $GS_DATA ]; then
+  host="`cat $GS_DATA/global.xml | grep "<proxyBaseUrl>" | sed 's#</\?proxyBaseUrl>##g' | sed 's#.*://##g' | sed 's#/.*##g'`"
+  admin="`cat $GS_DATA/security/users.properties | grep -v "^ *#" | grep ".*=.*, *ROLE_ADMINISTRATOR" | head -n 1`"
+  user="`echo $admin | sed 's/ *=.*//g'`"
+  pass="`echo $admin | sed 's/.*= *//g' | sed 's/,.*//g'`"
+
+  #save old values in order to replace them later
+  old_host=$host
+  old_user=$user
+  old_pass=$pass
+else
+  #host=`echo $(/sbin/ifconfig eth0 | awk -F: '/inet addr:/ {print $2}' | awk '{ print $1 }')`
+  host=""
+  user="admin"
+  pass="geoserver"
+fi
+
 exit=0
 set -e
 
@@ -38,9 +57,9 @@ menu() {
 
   Select an entry from the following list:
   ----------------------
-  1. Hostname or IP        : $myhost
-  2. Admin username        : $username
-  3. Admin password        : $password
+  1. Hostname or IP        : $host
+  2. Admin username        : $user
+  3. Admin password        : $pass
 
   9. Accept and continue
   0. Abort and quit
@@ -53,40 +72,77 @@ case "$menuchoice" in
     "1")
         printf "Please provide the IP address or hostname that GeoServer is accessed\n"
         printf "through publicly. This value is required in cases where GeoServer is\n"
-        printf "accessed through an external proxy.\n"
-        respond "hostname" "$myhost" "3"
-        myhost=$choice
-        myhost=`echo $myhost | sed s#http://##g | sed 's#/$##g'`
+        printf "accessed through an external proxy. Enter 'none' to leave value unset.\n"
+        respond "hostname" "$host" "3"
+        host=$choice
+        if [ -z $host ] || [ "$host" == "none" ]; then
+          host=""
+        else
+          host=`echo $host | sed s#http://##g | sed 's#/.*##g'`
+        fi
         ;;
 
     "2")
-        printf "Please choose a username for the GeoServer admin account.\n"
-        respond "username" "$username" "3"
-        username=$choice
+        printf "Please choose a user for the GeoServer admin account.\n"
+        respond "user" "$user" "3"
+        user=$choice
         ;;
 
     "3")
-        printf "Please choose a password for the GeoServer admin account.\n"
-        respond "password" "$password" "3"
-        password=$choice
+        printf "Please choose a pass for the GeoServer admin account.\n"
+        respond "pass" "$pass" "3"
+        pass=$choice
         ;;
 
     "9")
-        printf "Saving changes."
+        printf "Saving changes.\n"
         fin="1"
         #TODO configure a check to prevent rerunning this
 
-cat << EOF > /usr/share/opengeo-suite-data/geoserver_data/security/users.properties
-$username=$password,ROLE_ADMINISTRATOR
-# These are sample users you may uncomment if you want to test locking down wfs (see service.properties)
-#wfst=wfst,ROLE_WFS_READ,ROLE_WFS_WRITE
-#wfs=wfs,ROLE_WFS_READ
-EOF
+
+# update the proxy base url in global.xml
+GLOBAL_XML=$GS_DATA/global.xml
+if [ -z $host ]; then
+  # user is unsetting value, remove the old element if it exists
+  sed -i 's/.*<proxyBaseUrl>.*//g' $GLOBAL_XML
+
+elif [ $( grep "<proxyBaseUrl>" $GLOBAL_XML | wc -l ) != 0 ]; then
+
+  # user specified value and an old entry already exists
+
+  if [ ! -z $old_host ]; then
+    # the old value is not empty
+    sed -i "s#\(.*<proxyBaseUrl>.*\)$old_host\(.*\)#\1$host\2#g" $GLOBAL_XML
+  else
+    # the old value is empty
+    sed -i "s#\(.*\)<proxyBaseUrl>.*#\1<proxyBaseUrl>http://$host/geoserver</proxyBaseUrl>#g" $GLOBAL_XML
+  fi
+
+else 
+
+  # uset specified value but no element in file exists
+
+  sed -i "s#</global>#  <proxyBaseUrl>http://$host/geoserver</proxyBaseUrl>#g" $GLOBAL_XML
+  echo "</global>" >> $GLOBAL_XML
+
+fi
+
+# update user/pass in security/users.properties
+USERS_PROPS=$GS_DATA/security/users.properties
+if [ ! -z $old_user ]; then
+  if [ ! -z $old_pass ]; then
+    sed -i "s#$old_user *= *$old_pass#$user=$pass#g" $USERS_PROPS
+  else
+    sed -i "s#$old_user *=#$user=$pass#g" $USERS_PROPS
+  fi
+else
+  # no old user , append the new one
+  echo "$user=$pass,ROLE_ADMINISTRATOR" >> $USERS_PROPS
+fi
 
 if [ ! -d "/var/lib/tomcat5/webapps/geoserver" ]; then
        unzip  -o /var/lib/tomcat5/webapps/geoserver.war  -d /var/lib/tomcat5/webapps/geoserver  > /dev/null 2>&1
 fi
-sed -i -e "s/MYHOST/$myhost/g" /var/lib/tomcat5/webapps/geoserver/WEB-INF/web.xml
 
 if [ -e /etc/sysconfig/iptables ] && [ ! `cat /etc/sysconfig/iptables | grep 8080 |wc -l` ]; then
 	iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
@@ -107,7 +163,7 @@ fi
   esac
 }
 
-check_root
+#check_root
 while [ $fin -eq 0 ]; do
   menu
 done
