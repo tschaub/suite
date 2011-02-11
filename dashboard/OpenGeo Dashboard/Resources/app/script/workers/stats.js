1bf0b1decc701f561b01ac564f3561fd5db38121
tschaub <tschaub@fe66add1-021f-4af3-b75b-ea5154e73f91>
2011-02-11 18:14:11 +0000

    Backing out this change until GeoExplorer is updated.
    git-svn-id: http://svn.opengeo.org/suite/trunk@2192 fe66add1-021f-4af3-b75b-ea5154e73f91

--- dashboard/OpenGeo Dashboard/Resources/app/script/workers/stats.js
+++ dashboard/OpenGeo Dashboard/Resources/app/script/workers/stats.js
@@ -76,7 +76,7 @@ function getMapCount(config) {
     var resource = getResource(
         "http://" + config["suite_host"] + ":" + config["suite_port"] + "/geoexplorer/maps/"
     );
-    return resource && resource.maps.length;
+    return resource && resource.ids.length;
 }
 
 function collectStats(config) {    
