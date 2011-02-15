4ac7c299c339990a35a1bf6de72ca4c5d5756383
jdeolive <jdeolive@fe66add1-021f-4af3-b75b-ea5154e73f91>
2011-02-15 00:43:26 +0000

    added titles for anlytics pages
    git-svn-id: http://svn.opengeo.org/suite/trunk@2227 fe66add1-021f-4af3-b75b-ea5154e73f91

--- geoserver/analytics/src/main/java/org/opengeo/analytics/web/AnalyticsHomePage.java
+++ geoserver/analytics/src/main/java/org/opengeo/analytics/web/AnalyticsHomePage.java
@@ -6,36 +6,36 @@ import java.util.List;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
-import org.geoserver.monitor.Monitor;
+import org.apache.wicket.model.StringResourceModel;
 import org.geoserver.monitor.Query;
 import org.geoserver.monitor.web.MonitorBasePage;
-import org.geoserver.web.GeoServerApplication;
 
 public class AnalyticsHomePage extends MonitorBasePage {
 
     Query query;
+    String description;
     
     public AnalyticsHomePage() {
         query = new Query();
         
+        add(new Label("description", new PropertyModel<String>(this, "description")))
+        ;
         List<ITab> tabs = new ArrayList();
         tabs.add(new AbstractTab(new ResourceModel("summary")) {
             @Override
             public Panel getPanel(String panelId) {
+                description = description("summaryDescription");
                 return new SummaryPanel(panelId, query);
             }
         });
-//        tabs.add(new AbstractTab(new ResourceModel("requests")) {
-//            @Override
-//            public Panel getPanel(String panelId) {
-//                return new RequestPanel(panelId);
-//            }
-//        });
-        tabs.add(new AbstractTab(new ResourceModel("locations")) {
+        tabs.add(new AbstractTab(new ResourceModel("location")) {
             @Override
             public Panel getPanel(String panelId) {
+                description = description("locationDescription");
                 return new LocationPanel(panelId);
             }
         });
@@ -43,10 +43,15 @@ public class AnalyticsHomePage extends MonitorBasePage {
         tabs.add(new AbstractTab(new ResourceModel("performance")) {
             @Override
             public Panel getPanel(String panelId) {
+                description = description("performanceDescription");
                 return new PerformancePanel(panelId, query);
             }
         });
         
         add(new TabbedPanel("tabs", tabs));
+    }
+    
+    String description(String key) {
+        return new StringResourceModel(key, this, null).getObject();
     }
 }
