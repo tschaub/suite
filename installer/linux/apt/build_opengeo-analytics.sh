5e45f8b4e6d5dd5ffdc84c6a44e4dcb0efd7e895
jdeolive <jdeolive@fe66add1-021f-4af3-b75b-ea5154e73f91>
2011-02-17 21:34:11 +0000

    cleaned up opengeo-analytics package
    git-svn-id: http://svn.opengeo.org/suite/trunk@2259 fe66add1-021f-4af3-b75b-ea5154e73f91

--- installer/linux/apt/build_opengeo-analytics.sh
+++ installer/linux/apt/build_opengeo-analytics.sh
@@ -9,13 +9,12 @@ ANALYTICS=opengeosuite-ee-$BRANCH-$REV-analytics.zip
 get_file $BUILDS/$REPO_PATH/$ANALYTICS yes
 
 # clean out old files
-rm -rf opengeo-analytics/analytics
-mkdir opengeo-analytics/analytics
+rm -rf opengeo-analytics/*.jar
 
 # unpack
 mkdir tmp
 unzip files/$ANALYTICS -d tmp
-mv tmp/opengeosuite-*/*.jar opengeo-analytics/analytics
+mv tmp/opengeosuite-*/*.jar opengeo-analytics
 checkrc $? "unpacking analytics jars"
 rm -rf tmp
 
