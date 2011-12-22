a8df42201832a85d19ea5535d2fc9580f1bddc70
jdeolive <jdeolive@gmail.com>
2011-12-21 17:07:49 -0700

    adding separate line to update geoserver/externals/pom.xml

--- update_version.sh
+++ update_version.sh
@@ -11,4 +11,7 @@ TO="<version>$2<"
 
 find . -name externals -prune -o -name pom.xml -exec sed -i "" "s/$FROM/$TO/g" {} \;
 
+# we do geoserver/externals separately, since we pruned it out above
+sed -i "" "s/$FROM/$TO/g" geoserver/externals/pom.xml
+
 echo "Updated version strings in pom.xml files from $1 to $2."
