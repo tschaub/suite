aea184e11e7da359bc9aa9c705ef865ac63f5b69
Ian Schneider <ischneider@opengeo.org>
2012-01-06 13:55:43 -0700

    add a progress endpoint for tracking progress of items, add means to count features in DataStoreFormat for progress tracking, allow parameter to ImportResource to specify async processing, error handling cleanup

--- geoserver/importer/src/main/java/org/opengeo/data/importer/VectorFormat.java
+++ geoserver/importer/src/main/java/org/opengeo/data/importer/VectorFormat.java
@@ -22,4 +22,9 @@ public abstract class VectorFormat extends DataFormat {
      * Disposes the reader for the specified import item.  
      */
     public abstract void dispose(FeatureReader reader, ImportItem item) throws IOException;
+    
+    /**
+     * Get the number of features from the data for the specified import item.
+     */
+    public abstract int getFeatureCount(ImportData data, ImportItem item) throws IOException;
 }
