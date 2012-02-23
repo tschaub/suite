5263bcb82444aff9f6d74054b1914f03f6526c2e
jdeolive <jdeolive@gmail.com>
2012-02-23 11:33:53 -0700

    skipping secondary git pull if building from a tag

--- build/main/suite/build.sh
+++ build/main/suite/build.sh
@@ -42,7 +42,12 @@ export MAVEN_OPTS=-Xmx256m
 cd repo
 if [ ! -z $REV ]; then
   git checkout $REV
-  git pull origin $REV
+
+  # if this rev is a tag don't pull
+  if [ "$( git tag | grep $REV )" != $REV ]; then
+     git pull origin $REV
+  fi
+
   git submodule update --init --recursive
 fi
 
